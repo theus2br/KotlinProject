7ac8428992c4c2a7e69bfad0b0191093fb7f1d58
Matheus <matlivebr@hotmail.com>
2020-12-19 14:06:00 -0300

    Melhorando o codigo de lista

--- Aprendizado/src/Main.kt
+++ Aprendizado/src/Main.kt
@@ -2,19 +2,16 @@ import javax.swing.JOptionPane
 
 fun main() {
     val cliente = Cliente()
-    val createClient = AdicionarPessoa(cliente)
-    val createSecondClient = Cliente("Mariana bostinha", 12)
-    val createListClients = Clientes(listOf(createClient, createSecondClient))
+    val clientes = Clientes()
 
-    println(createListClients)
+    val createClient = AdicionarPessoas(clientes, cliente)
+    println(createClient)
 }
 
-fun listaPessoas(clientes: Clientes, cliente: Cliente){
-
-    clientes
-}
-fun AdicionarPessoa(cliente: Cliente): Cliente{
+fun AdicionarPessoas(clientes: Clientes, cliente:Cliente): Clientes{
     cliente.nome = JOptionPane.showInputDialog("Digite seu nome");
     cliente.idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade"))
-    return cliente
+
+    listOf(clientes)
+    return clientes
 }
